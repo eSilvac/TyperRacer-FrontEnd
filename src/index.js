e5f8b186b8ad176592dec5e3d45b9e1789f330e2
Esteban Silva <esilvac59@gmail.com>
2019-11-15 09:11:01 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import React from 'react';
+
+// import store from './js/store/index';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+// import { verifyUser } from './js/actionsCreator/user/index';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// store.dispatch(verifyUser());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+
 serviceWorker.unregister();
